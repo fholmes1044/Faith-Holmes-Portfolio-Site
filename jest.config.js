c1bb6d0e6badf041d7be8554d83998035c4ee282
+ <+>
2023-10-04 19:58:55 -0400

    deployment updates

--- jest.config.js
+++ jest.config.js
@@ -11,9 +11,23 @@
 //       },
 //     },
 //   };
-export default {
-    preset: 'react-app',
+// export default {
+//     preset: 'react-app',
+//     testEnvironment: 'jsdom',
+//     transformIgnorePatterns: ['node_modules/(?!(babel-jest|@babel/preset-env)/)'],
+//   };
+module.exports = {
+    preset: 'react-scripts',
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+    },
     testEnvironment: 'jsdom',
-    transformIgnorePatterns: ['node_modules/(?!(babel-jest|@babel/preset-env)/)'],
+    transformIgnorePatterns: [
+      '/node_modules/(?!(babel-jest|@babel/preset-env)/)',
+    ],
+    globals: {
+      'ts-jest': {
+        useESM: true,
+      },
+    },
   };
-  
