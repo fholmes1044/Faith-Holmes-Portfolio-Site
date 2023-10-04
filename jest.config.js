6b84bf519e6a8610a1b7eb28df1b71bb4736fb66
+ <+>
2023-10-04 19:26:51 -0400

    updated jest.config.js after installing an esm package

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 // jest.config.js
 export default {
-    // Other Jest configurations...
     transform: {
       '^.+\\.jsx?$': 'babel-jest',
     },
     testEnvironment: 'node',
     extensionsToTreatAsEsm: ['.jsx'],
-    setupFiles: ['esm'],
     globals: {
       'ts-jest': {
         useESM: true,
